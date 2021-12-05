36630d416e394d42d8302ff67b4c13b6f5680aec
Jacob <yawjacob@yahoo.com>
2021-12-05 17:24:09 -0600

    adding buttons/messing with css rules

--- index.js
+++ index.js
@@ -3,6 +3,22 @@ document.addEventListener('DOMContentLoaded', function(){
 
 	fetch('https://dog.ceo/api/breeds/image/random/5')
 	.then(res => res.json())
-	.then(data => console.log(data))
+	.then(handlePictures)
 
 })//end of DOMContentLoaded
+
+function handlePictures(jsonObject){
+	let cardContainer = document.querySelector("#card-container")
+	let arrOfURLS = jsonObject.message
+	arrOfURLS.forEach(url => {
+		cardContainer.innerHTML += createImage(url)
+	})
+}
+
+function createImage(url){
+	let card = document.createElement('img')
+	card.className = ".cards"
+	console.log(card.className)
+	card = `<img src="${url}"/>`
+	return card
+}
