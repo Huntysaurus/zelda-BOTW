8a3a7f25d073ac83968140ec7e2d5ff2479719e8
Jacob <yawjacob@yahoo.com>
2021-12-08 21:22:13 -0600

    made all buttons reset/show new images

--- index.js
+++ index.js
@@ -1,20 +1,39 @@
 
 document.addEventListener('DOMContentLoaded', function(){
 
-	fetch('https://botw-compendium.herokuapp.com/api/v2/category/monsters')
-	.then(res => res.json())
-	.then(handleMonsterPics)
+	const creatBut = document.querySelector("#creatures")
+	const equipBut = document.querySelector("#equipment")
+	const materBut = document.querySelector("#materials")
+	const monstBut = document.querySelector("#monsters")
+	const treasBut = document.querySelector("#treasure")
+
+	console.log(URL)
+
+	console.log(creatBut, equipBut, materBut, monstBut, treasBut)
+
+	// fetch('https://botw-compendium.herokuapp.com/api/v2/category/monsters')
+	// .then(res => res.json())
+	// .then(handleMonsterPics)
+
+	creatBut.addEventListener('click', getCreatures)
+	equipBut.addEventListener('click', getEquipment)
+	materBut.addEventListener('click', getMaterials)
+	monstBut.addEventListener('click', getMonsters)
+	treasBut.addEventListener('click', getTreasure)
 
 })//end of DOMContentLoaded
+
+const URL = 'https://botw-compendium.herokuapp.com/api/v2/category'
 
 //grab the images of all the monsters
 function handleMonsterPics(monsters){
 	let imageContainer = document.querySelector("#image-container")
+	imageContainer.innerHTML = ''
 	monsters.data.forEach(monster => imageContainer.innerHTML += createImage(monster.image))
 	console.log(monsters)
 }
 
-//create and append monster images to the DOM
+//create and append images to the DOM
 function createImage(url){
 	let image = document.createElement('img')
 	image = `<img class="image" src="${url}"/>`
@@ -25,3 +44,32 @@ function createImage(url){
 	return image
 }
 
+function getCreatures() {
+	fetch(`${URL}/monsters`)
+	.then(res => res.json())
+	.then(handleMonsterPics)
+}
+
+function getEquipment() {
+	fetch(`${URL}/equipment`)
+	.then(res => res.json())
+	.then(handleMonsterPics)
+}
+
+function getMaterials() {
+	fetch(`${URL}/materials`)
+	.then(res => res.json())
+	.then(handleMonsterPics)
+}
+
+function getMonsters() {
+	fetch(`${URL}/monsters`)
+	.then(res => res.json())
+	.then(handleMonsterPics)
+}
+
+function getTreasure() {
+	fetch(`${URL}/treasure`)
+	.then(res => res.json())
+	.then(handleMonsterPics)
+}
